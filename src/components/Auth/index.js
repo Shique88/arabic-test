d6de5f8b975fc906bdd38276541e0e9205b7fbb7
Shique88 <40884166+Shique88@users.noreply.github.com>
2025-05-01 13:18:05 +0300

    Create index.js

---
+++ src/components/Auth/index.js
@@ -0,0 +1,4 @@
+// src/components/Auth/index.js
+export { default as Login } from './Login';
+export { default as Register } from './Register';
+export { default as UserProfile } from './UserProfile';
