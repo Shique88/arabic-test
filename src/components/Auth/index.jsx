52482c79625371e0950bc1c17e1d3a13beb6af69
Shique88 <40884166+Shique88@users.noreply.github.com>
2025-05-01 15:11:02 +0300

    Delete src/components/Auth/index.jsx

--- src/components/Auth/index.jsx
+++
@@ -1,4 +0,0 @@
-// src/components/Auth/index.js
-export { default as Login } from './Login';
-export { default as Register } from './Register';
-export { default as UserProfile } from './UserProfile';
